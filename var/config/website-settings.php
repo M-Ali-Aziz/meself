7dd55061b98ba69e5b710fba59b536db83d64751
Mohammed Ali <mohammed_ali.aziz_assi@ldc.lu.se>
2020-10-08 10:23:42 +0200

    settings,  class

--- var/config/website-settings.php
+++ var/config/website-settings.php
@@ -1,14 +1,5 @@
 <?php 
 
 return [
-    1 => [
-        "id" => 1,
-        "name" => "membersParentId",
-        "language" => "",
-        "type" => "text",
-        "data" => "2",
-        "siteId" => 0,
-        "creationDate" => 0,
-        "modificationDate" => 1600347987
-    ]
+
 ];
