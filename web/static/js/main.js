91b811bb9753fa7ed1d0f498150fd8477feb56b4
Mohammed Ali <mohammed_ali.aziz_assi@ldc.lu.se>
2020-10-08 10:22:26 +0200

    js, css

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,3 +1,50 @@
+/* -------------------------------------------------
+ * CKEditor Config
+ * ------------------------------------------------- */
+ClassicEditor
+    .create( document.querySelector( '#editor' ), {
+        toolbar: ["undo", "redo", "|", "heading", "|", "bold", "italic", "blockQuote", "numberedList", "bulletedList", "|", "mediaEmbed"],
+        mediaEmbed: {previewsInData: true}
+    } )
+    .then( editor => {
+        // console.log( editor );
+        // console.log( Array.from( editor.ui.componentFactory.names()));
+    } )
+    .catch( error => {
+        console.log( error );
+    } );
+
+
+/* -------------------------------------------------
+ * SHOW UPLOADED IMAGE
+ * ------------------------------------------------- */
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#imageResult')
+                .attr('src', e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+    $('#upload').on('change', function () {
+        readURL(input);
+    });
+});
+
+/* -------------------------------------------------
+ * SHOW UPLOADED IMAGE NAME
+ * ------------------------------------------------- */
+var input = document.getElementById( 'upload' );
+var infoArea = document.getElementById( 'upload-label' );
+
+input.addEventListener( 'change', showFileName );
+function showFileName( event ) {
+  var input = event.srcElement;
+  var fileName = input.files[0].name;
+  infoArea.textContent = 'File name: ' + fileName;
+}
